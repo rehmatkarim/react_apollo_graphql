911896683aca1da085ebf70d3bff696f6ae8a05a
rehmatkarim <p146120@gmail.com>
2021-05-27 15:58:47 -0400

    apollo client configured for auth token

--- src/index.js
+++ src/index.js
@@ -10,11 +10,22 @@ import {
   InMemoryCache
 } from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
+import { setContext } from '@apollo/client/link/context';
+import { AUTH_TOKEN } from './constant';
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000'
 });
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 ReactDOM.render(
@@ -22,7 +33,7 @@ ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
-  </BrowserRouter>,
+   </BrowserRouter>,
   document.getElementById('root')
 );
 
